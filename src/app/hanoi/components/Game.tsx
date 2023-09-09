e790c0e7a0f81773a3ddbc1b7837faca022b8600
kyuki3rain <kyuki3rain@gmail.com>
2023-09-09 23:30:26 +0900

    undo / redoを実装

--- src/app/hanoi/components/Game.tsx
+++ src/app/hanoi/components/Game.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useHeight } from "../states/height";
 import Tower from "./Tower";
+import { Button } from "@nextui-org/button";
+import RedoIcon from "../icons/RedoIcon";
+import UndoIcon from "../icons/UndoIcon";
 
 type Props = {
   height: number;
@@ -21,27 +24,53 @@ function GameView({ height }: Props) {
     b: [] as number[],
     c: [] as number[],
   });
+  const [logs, setLogs] = useState<{ from: TowerType; to: TowerType }[]>([]);
+  const [index, setIndex] = useState(0);
   const [from, setFrom] = useState<TowerType | null>(null);
-  const [count, setCount] = useState(0);
 
-  const move = (from: TowerType, to: TowerType) => {
-    const fromState = state[from];
-    const toState = state[to];
-    if (fromState.length === 0) return;
+  const move = useCallback(
+    (from: TowerType, to: TowerType, logging: boolean = true) => {
+      const fromState = state[from];
+      const toState = state[to];
+      if (fromState.length === 0) return;
 
-    toState.push(fromState.pop()!);
-    setState({ ...state, [from]: fromState, [to]: toState });
-    setCount(count + 1);
-  };
+      toState.push(fromState.pop()!);
+      setState({ ...state, [from]: fromState, [to]: toState });
+      if (logging === true) {
+        setLogs([...logs.slice(0, index), { from, to }]);
+        setIndex(index + 1);
+      }
+    },
+    [state, logs, index]
+  );
 
-  const onClick = (tower: TowerType) => {
-    if (from === null) {
-      setFrom(tower);
-    } else {
-      move(from, tower);
-      setFrom(null);
-    }
-  };
+  const undo = useCallback(() => {
+    if (index === 0) return;
+
+    const log = logs[index - 1];
+    move(log.to, log.from, false);
+    setIndex(index - 1);
+  }, [index, logs, move]);
+
+  const redo = useCallback(() => {
+    if (index === logs.length) return;
+
+    const log = logs[index];
+    move(log.from, log.to, false);
+    setIndex(index + 1);
+  }, [index, logs, move]);
+
+  const onClick = useCallback(
+    (tower: TowerType) => {
+      if (from === null) {
+        setFrom(tower);
+      } else {
+        move(from, tower);
+        setFrom(null);
+      }
+    },
+    [from, move]
+  );
 
   return (
     <div className="flex flex-col items-center">
@@ -65,7 +94,27 @@ function GameView({ height }: Props) {
           className="bg-blue-100"
         ></Tower>
       </div>
-      <div className="text-4xl">Count: {count}</div>
+      <div className="flex flex-row">
+        <Button
+          isIconOnly
+          color="danger"
+          aria-label="Like"
+          className="mx-4"
+          onClick={undo}
+        >
+          <UndoIcon />
+        </Button>
+        <div className="text-4xl">Count: {index}</div>
+        <Button
+          isIconOnly
+          color="danger"
+          aria-label="Like"
+          className="mx-4"
+          onClick={redo}
+        >
+          <RedoIcon />
+        </Button>
+      </div>
     </div>
   );
 }
