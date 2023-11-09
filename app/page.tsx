7456399750f8f9add7b392a9369de53000f2dfa9
kyuki3rain <kyuki3rain@gmail.com>
2023-11-09 13:29:12 +0900

    TOPページのスタイルを調整

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,18 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Portfolio Site of kyuki3rain / coloname33</div>
       <div className="flex flex-col items-center">
-        <Link href="/brick-breaker">ブロック崩し</Link>
-        <Link href="/hanoi">ハノイの塔</Link>
+        <Link
+          href="/brick-breaker"
+          className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+        >
+          ブロック崩し
+        </Link>
+        <Link
+          href="/hanoi"
+          className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+        >
+          ハノイの塔
+        </Link>
       </div>
     </main>
   );
