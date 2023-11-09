71f82331bb20abeb649f63dcc956b64ccb7e5cec
kyuki3rain <kyuki3rain@gmail.com>
2023-11-09 13:20:42 +0900

    ブロック崩しを追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Portfolio Site of kyuki3rain / coloname33</div>
-      <Link href="/hanoi">Tower of Hanoi</Link>
+      <div className="flex flex-col items-center">
+        <Link href="/brick-breaker">ブロック崩し</Link>
+        <Link href="/hanoi">ハノイの塔</Link>
+      </div>
     </main>
   );
 }
