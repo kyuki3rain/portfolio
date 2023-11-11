27192dcfd78afe56dc246ddf409fc6202be2a485
kyuki3rain <kyuki3rain@gmail.com>
2023-11-11 14:11:40 +0900

    routeを修正

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,6 @@
-import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import authOptions from "@/options";
 
-const authHandler: NextApiHandler = (req, res) => {
-  console.log(req.method, req.url);
-  return NextAuth(req, res, authOptions);
-};
+const authHandler = NextAuth(authOptions);
 
 export { authHandler as GET, authHandler as POST };
