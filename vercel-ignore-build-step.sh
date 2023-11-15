fd78d966e3f8fe6785a4b47990d977675055410d
kyuki3rain <kyuki3rain@gmail.com>
2023-11-15 09:23:22 +0900

    elif

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -8,7 +8,7 @@ if [[ "$VERCEL_ENV" == "production" ]] ; then
   echo "✅ - Build can proceed"
   exit 1;
 
-else if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
+elif [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
